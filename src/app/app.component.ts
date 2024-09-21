7426e9519acafeec1a30cd25570357eecab277e0
iarmolla <ivan.armolla@gmail.com>
2024-09-21 02:27:49 -0300

    feat: add project list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./components/about/about.component";
+import { SkillsComponent } from "./components/skills/skills.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { ProjectListComponent } from "./components/project-list/project-list.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, AboutComponent, SkillsComponent, NavbarComponent, ProjectListComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor() {
+
+  }
 }
