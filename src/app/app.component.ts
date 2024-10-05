aac38056164472eb4271f7bbff5bc7f77cf118e4
iarmolla <ivan.armolla@gmail.com>
2024-10-05 02:16:39 -0300

    feat: dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './shared/components/about/about.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { ProjectListComponent } from './shared/components/project-list/project-list.component';
 import { SkillsComponent } from './shared/components/skills/skills.component';
 import { ModalComponent } from './shared/components/modal/modal.component';
+import { ToggleComponent } from '@ngx-dark-mode-toggle/core';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent, SkillsComponent, NavbarComponent, ProjectListComponent, ModalComponent],
+  imports: [RouterOutlet, AboutComponent, SkillsComponent, NavbarComponent, ProjectListComponent, ModalComponent, ToggleComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'portfolio';
 
-  constructor() {
-
-  }
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ElementRef, inject, PLATFORM_ID, ViewChild } from '@angular/core';
+import { ToggleComponent } from '@ngx-dark-mode-toggle/core';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ToggleComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  showDarkMode: string = 'false'
+  isBrowser: boolean = false
+  platformId = inject(PLATFORM_ID)
+  constructor() {
+    this.isBrowser = isPlatformBrowser(this.platformId)
+  }
+  ngAfterViewInit() {
+    if(!this.isBrowser) return
+    this.getStorage()
+  }
+  getStorage() {
+    const darkModeStorage = localStorage.getItem('darkModeEnabled')
+    this.showDarkMode = darkModeStorage as unknown as string
+  }
   openCv() {
-    
+    window.open('../../../../assets/ivanarmolla.pdf', '_blank')
+  }
+  darkMode() {
+    const btn = document.querySelector("input[type='checkbox']") as HTMLElement
+    if(btn) btn?.click()
+    this.getStorage()
   }
 }
